0d3161917238b8f417a86a51a133d10942166752
Bohdan Shtepan <winsatana@ya.ru>
2015-11-24 09:08:03 +0200

    Adds Gruntfile.js, package.json, travis cfg

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+/**
+ * grunt-iife - a Grunt plugin for wrapping JavaScript code in IIFEs.
+ * https://github.com/virtyaluk/grunt-iife
+ *
+ * Copyright (c) 2015 Bohdan Shtepan
+ * http://modern-dev.com/
+ *
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-eslint');
+
+    grunt.initConfig({
+        eslint: {
+            target: 'tasks/**/*.js'
+        }
+    });
+
+    grunt.registerTask('default', ['eslint']);
+};
