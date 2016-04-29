874604b6ffec0c6333ff01717613f06364485f95
Bohdan Shtepan <winsatana@ya.ru>
2016-04-29 22:20:00 +0300

    Bump dependecies; Refactoring; Code improvements.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
  * grunt-iife - a Grunt plugin for wrapping JavaScript code in IIFEs.
  * https://github.com/virtyaluk/grunt-iife
  *
- * Copyright (c) 2015 Bohdan Shtepan
+ * Copyright (c) 2016 Bohdan Shtepan
  * http://modern-dev.com/
  *
  * Licensed under the MIT license.
